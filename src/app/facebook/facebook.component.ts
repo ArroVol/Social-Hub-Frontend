ea15bc8aaf2b154f8070a1e23dd31b6e8cefa5ff
jmanalo26 <63303459+jmanalo26@users.noreply.github.com>
2021-02-17 16:17:39 -0600

    Added in Basic Facebook Login functionality, no logout yet

--- src/app/facebook/facebook.component.ts
+++ src/app/facebook/facebook.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var FB: any;
 @Component({
   selector: 'app-facebook',
   templateUrl: './facebook.component.html',
@@ -10,6 +11,44 @@ export class FacebookComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // tslint:disable-next-line:only-arrow-functions
+    (window as any).fbAsyncInit = function() {
+      FB.init({
+        appId      : '2959296474304941',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v9.0'
+      });
+      FB.AppEvents.logPageView();
+    };
+
+    // tslint:disable-next-line:only-arrow-functions
+    (function(d, s, id){
+      // tslint:disable-next-line:one-variable-per-declaration prefer-const
+      let js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return; }
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+
+  submitLogin(){
+    console.log('User attempting to log in to Facebook');
+    // FB.login();
+    FB.login((response) =>
+    {
+      console.log('submitLogin', response);
+      if (response.authResponse)
+      {
+        console.log('User Logged in Successfully');
+      }
+      else
+      {
+        console.log('User login failed');
+      }
+    });
+
   }
 
 }
