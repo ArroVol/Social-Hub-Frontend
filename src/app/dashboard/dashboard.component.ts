7f29c9068b9287bae261716ea3b2a50cda2b4057
jmanalo26 <63303459+jmanalo26@users.noreply.github.com>
2021-04-05 20:53:19 -0500

    4/5 Dashboard login in progress, dashboard ts redone to fit other projects

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {FacebookService} from '../service/facebook.service';
 import {FacebookUser} from '../model/facebook/FacebookUser';
 import {FacebookPhotos} from '../model/facebook/FacebookPhotos';
-import {FacebookComponent} from '../facebook/facebook.component';
 import { ActivatedRoute } from '@angular/router';
+import {FacebookLogin} from '../model/facebook/FacebookLogin';
+import {FacebookPages} from '../model/facebook/FacebookPages';
 
 
 @Component({
@@ -14,14 +15,16 @@ import { ActivatedRoute } from '@angular/router';
 /**
  * The Class for the dashboard component.
  */
-export class DashboardComponent extends FacebookComponent implements OnInit {
+export class DashboardComponent implements OnInit {
   facebookUser: FacebookUser;
+  facebookLogin: FacebookLogin;
+  facebookPhotos: FacebookPhotos;
+  facebookPages: FacebookPages;
+  verificationCode: string;
+  constructor(protected route: ActivatedRoute, protected facebookService: FacebookService) {
+  }
+
   ngOnInit(): void {
-    if (this.facebookUser !== undefined){
-      console.log(this.facebookUser);
-      document.getElementById('fbCard').style.display = 'block';
-      document.getElementById('fbCardLogin').style.display = 'none';
-    }
     this.route.queryParams.subscribe(params => {
       let code = params['code'];
       if (!code){
@@ -32,14 +35,42 @@ export class DashboardComponent extends FacebookComponent implements OnInit {
         this.facebookService.sendVerificationCode(this.verificationCode)
           .subscribe(result => {
             console.log('Result: ', result);
-            this.getUsername();
-            this.getPhotos();
-            this.getPages();
+            this.getFBUsername();
+            this.getFBPhotos();
+            this.getFBPages();
             document.getElementById('fbCard').style.display = 'block';
             document.getElementById('fbCardLogin').style.display = 'none';
           });
       }
     });
+  }
+
+  loginFB(){
+    this.facebookService.login().subscribe(loginDialogURL => {
+      this.facebookLogin = loginDialogURL;
+    });
+    window.location.href = this.facebookLogin.loginDialogURL;
+  }
+
+  getFBUsername(){
+    this.facebookService.getUserName().subscribe(facebookUser => {
+      this.facebookUser = facebookUser;
+    });
+    console.log('Get username method called');
+  }
+
+  getFBPhotos(){
+    this.facebookService.getPhotos().subscribe(facebookPhotos => {
+      this.facebookPhotos = facebookPhotos;
+    });
+    console.log('Get photos method called');
+  }
+
+  getFBPages(){
+    this.facebookService.getPages().subscribe(facebookPages => {
+      this.facebookPages = facebookPages;
+    });
+    console.log('Get pages method called');
   }
 
 }
